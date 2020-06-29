206c76e19e89c40f12435811496805e6328ccbbf
ISA002 <ggeasy1710@gmail.com>
2020-06-29 22:10:29 +0400

    Updates

---
+++ precache-manifest.4f7cb6c7c468788908d6bb5801157867.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c47ff540d39f5530a6ae9efd330cac5",
+    "url": "/snp-task-2-react/index.html"
+  },
+  {
+    "revision": "349913846da187d6b21c",
+    "url": "/snp-task-2-react/static/css/main.d8fbab31.chunk.css"
+  },
+  {
+    "revision": "e3ff18377f62c65087cc",
+    "url": "/snp-task-2-react/static/js/2.decbfb8a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/snp-task-2-react/static/js/2.decbfb8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "349913846da187d6b21c",
+    "url": "/snp-task-2-react/static/js/main.671c3534.chunk.js"
+  },
+  {
+    "revision": "7d5e7529138d72aae98e",
+    "url": "/snp-task-2-react/static/js/runtime-main.5778b798.js"
+  },
+  {
+    "revision": "5ebb24ee1112dd9562629375c387879a",
+    "url": "/snp-task-2-react/static/media/Roboto-Black.5ebb24ee.ttf"
+  },
+  {
+    "revision": "87f1860d77403a979890a24f1783d05a",
+    "url": "/snp-task-2-react/static/media/noAvatar1.87f1860d.png"
+  }
+]);
